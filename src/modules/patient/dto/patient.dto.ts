9b65992b72ce2a15018f0cbbcec2eb060d9bb86a
WhiteAdam <jurchenko.a@gmail.com>
2023-03-30 00:48:11 +0200

    chore: pre-commit configuration

--- src/modules/patient/dto/patient.dto.ts
+++ src/modules/patient/dto/patient.dto.ts
@@ -6,7 +6,7 @@ import {
   IsEnum,
 } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-import { Gender } from 'src/enams/Gender';
+import { Gender } from 'src/enums/Gender';
 
 export class PatientDto {
   @ApiProperty({ description: 'First name of the patient', example: 'Adam' })
