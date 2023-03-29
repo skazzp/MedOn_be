8e60bdc19f5f4fa9266c3cbe19f80a9c403e777e
WhiteAdam <jurchenko.a@gmail.com>
2023-03-29 13:01:39 +0200

    chore: initial commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
+
+@Injectable()
+export class AuthService {
+  singin(dto: AuthDto) {
+    return dto;
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  await app.listen(app.get(ConfigService).get('PORT'));
+}
+bootstrap();
