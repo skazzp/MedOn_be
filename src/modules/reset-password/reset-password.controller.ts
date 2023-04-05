dc9434aff4e28baaab47996f6512d0c98c6384e7
Matheus <matheus-rodrigues37@live.com>
2023-04-05 11:00:38 -0300

    MED-47 feat: add getDoctorByEmail

--- src/modules/reset-password/reset-password.controller.ts
+++ src/modules/reset-password/reset-password.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Doctor } from 'src/typeorm/entities/Doctor';
 import { ResetPasswordService } from './reset-password.service';
+import { ResetPasswordDto } from './dto/reset-password.dto';
 
+@ApiTags('reset-password')
 @Controller('reset-password')
 export class ResetPasswordController {
   constructor(private readonly resetPasswordService: ResetPasswordService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Get email of forgot password doctor' })
+  async getDoctorByEmail(@Body() dto: ResetPasswordDto): Promise<Doctor> {
+    const doctor = await this.resetPasswordService.findDoctorByEmail(dto.email);
+    return doctor;
+  }
+
+  // @Post()
+  // @ApiOperation({ summary: 'Create patients' })
+  // createPatient(@Body() dto: ResetPasswordDto): Promise<Doctor> {
+  //   return this.resetPasswordService.createPatient(dto);
+  // }
 }
