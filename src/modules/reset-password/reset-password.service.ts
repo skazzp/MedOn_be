bf70e8f9878b6e93feb3ebe44fb5fb4035243478
Matheus <matheus-rodrigues37@live.com>
2023-04-05 11:02:41 -0300

    MED-47 feat: add findDoctorByEmail

--- src/modules/reset-password/reset-password.service.ts
+++ src/modules/reset-password/reset-password.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Doctor } from 'src/typeorm/entities/Doctor';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ResetPasswordService {}
+export class ResetPasswordService {
+  constructor(
+    @InjectRepository(Doctor) private doctorRepo: Repository<Doctor>,
+  ) {}
+
+  findDoctorByEmail(email: string): Promise<Doctor> {
+    return this.doctorRepo.findOne({ where: { email } });
+  }
+}
