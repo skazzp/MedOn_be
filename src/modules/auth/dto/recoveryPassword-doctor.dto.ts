c6b59ed1d4234e41d8c27bf1266668f561a50198
Matheus <matheus-rodrigues37@live.com>
2023-04-11 17:50:57 -0300

    MED-52 fix: dto's to standard

--- src/modules/auth/dto/recoveryPassword-doctor.dto.ts
+++
@@ -1,7 +0,0 @@
-export interface ForgetPasswordDoctorDto {
-  email: string;
-}
-
-export interface ResetPasswordDoctorDto {
-  newPassword: string;
-}
---
+++ src/modules/auth/dto/resetPassword-doctor.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+export class ResetPasswordDoctorDto {
+  @ApiProperty({
+    description: 'Email of the doctor',
+    example: 'dr-jenner@gmail.com',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
+    description: "Doctor's password",
+    example: 'TyuiOp123!',
+  })
+  @MinLength(6)
+  @IsString()
+  @Matches(/(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password too weak!',
+  })
+  newPassword: string;
+}
