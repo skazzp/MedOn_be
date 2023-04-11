ed1c79336553d1d3d487b7bddf4223393fbaac7e
Matheus <matheus-rodrigues37@live.com>
2023-04-11 17:58:36 -0300

    MED-52 chore: create email module

---
+++ src/modules/email/email.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { SentMessageInfo } from 'nodemailer';
+import { MailerService } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class EmailService {
+  constructor(private email: MailerService, private config: ConfigService) {}
+
+  public sendConfirmationLink(
+    to: string,
+    link: string,
+  ): Promise<SentMessageInfo> {
+    return this.email.sendMail({
+      to,
+      from: this.config.get('EMAIL_SENDER'),
+      subject: 'Please confirm your registration in MedOn System',
+      template: 'welcome',
+      context: {
+        link,
+      },
+    });
+  }
+
+  public sendForgetPasswordLink(
+    to: string,
+    link: string,
+  ): Promise<SentMessageInfo> {
+    return this.email.sendMail({
+      to,
+      from: this.config.get('EMAIL_SENDER'),
+      subject: 'Please update your password in MedOn System',
+      template: 'reset',
+      context: {
+        link,
+      },
+    });
+  }
+}
