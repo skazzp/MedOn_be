5efa5714ac89b5f8f830de35f4cd700270013917
eduardojigub <eduardobrunoit@gmail.com>
2023-04-10 21:41:57 -0300

    fix: callbackUrl string

--- src/utils/google.strategy.ts
+++ src/utils/google.strategy.ts
@@ -9,7 +9,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: configService.get('GOOGLE_CLIENT_ID'),
       clientSecret: configService.get('GOOGLE_CLIENT_SECRET'),
-      callbackURL: 'http://localhost:3000/auth/google/redirect',
+      callbackURL: 'http://localhost:3001/api/auth/google/redirect',
       scope: ['email', 'profile'],
     });
   }
