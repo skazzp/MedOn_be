5e5e57962aa8ecd99048c0fb3b45209e7d440896
Matheus <matheus-rodrigues37@live.com>
2023-04-10 23:48:13 -0300

    MED-52 chore: improve error messages

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -16,7 +16,9 @@ export class AuthGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
-      throw new UnauthorizedException('Unauthorized');
+      throw new UnauthorizedException(
+        'Not possible to do the action because the link is corrupted',
+      );
     }
     try {
       const payload = await this.jwtService.verifyAsync(token, {
@@ -24,7 +26,9 @@ export class AuthGuard implements CanActivate {
       });
       request['doctor'] = payload;
     } catch {
-      throw new UnauthorizedException('Unauthorized');
+      throw new UnauthorizedException(
+        'Not possible to do the action because the link has expired',
+      );
     }
     return true;
   }
