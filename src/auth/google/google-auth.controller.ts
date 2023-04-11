b86341e6304e5e328b044ffb3caf4da732c6ba3d
eduardojigub <eduardobrunoit@gmail.com>
2023-04-10 21:57:17 -0300

    feat: add Guards to routes

--- src/auth/google/google-auth.controller.ts
+++ src/auth/google/google-auth.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { GoogleAuthGuard } from '../guards/Guards';
 
 @Controller('auth')
 export class GoogleAuthController {
   // this method will handle the login from users.
   @Get('google/login') // endpoint: api/auth/google/login
+  @UseGuards(GoogleAuthGuard)
   handleLogin() {
     return { msg: 'Google Authentication' };
   }
 
   // it has to match the authorized redirect url in google developer console
   @Get('google/redirect') // endpoint: api/auth/google/redirect
+  @UseGuards(GoogleAuthGuard)
   handleRedirect() {
     return { msg: 'OK' };
   }
