d7ea39af04f22d713fcd048b7c744629aff26b06
Matheus <matheus-rodrigues37@live.com>
2023-04-17 19:40:10 -0300

    MED-53 chore: files from others to make code work

---
+++ src/common/interfaces/userProfileResponses.ts
@@ -0,0 +1,34 @@
+import { HttpStatus } from '@nestjs/common';
+import { Request } from 'express';
+
+export interface IProfileResponse {
+  statusCode: HttpStatus;
+  data: IProfile;
+}
+
+export interface IProfileRequest extends Request {
+  user: {
+    email: string;
+    userId: number;
+  };
+}
+
+export interface IProfile {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  city: string | null;
+  country: string | null;
+  photo: string | null;
+  dateOfBirth: Date | null;
+  role: string | null;
+  isVerified: boolean;
+  timeZone: string | null;
+  specialityId: number | null;
+}
+
+export interface IUpdateProfileResponse {
+  statusCode: HttpStatus;
+  message: string;
+}
