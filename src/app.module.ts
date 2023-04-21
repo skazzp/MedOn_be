601d7c5b650b2a7e62fc95fdfe7048d0ce933af5
WhiteAdam <jurchenko.a@gmail.com>
2023-04-21 23:51:55 +0200

    feat: add /patients routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { AuthModule } from '@modules/auth/auth.module';
 import { EmailModule } from '@modules/email/email.module';
 import { UserModule } from '@modules/user/user.module';
 import { SpecialitiesModule } from '@modules/specialities/specialities.module';
+import { PatientsModule } from '@modules/patients/patients.module';
 
 @Module({
   imports: [
@@ -34,6 +35,7 @@ import { SpecialitiesModule } from '@modules/specialities/specialities.module';
     UserModule,
     SpecialitiesModule,
     UserModule,
+    PatientsModule,
   ],
 })
 export class AppModule {}
---
+++ src/modules/patients/dto/create-patient.dto.ts
@@ -0,0 +1,84 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsDateString,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+import { Gender } from '@common/enums';
+
+export class CreateUserDto {
+  @ApiProperty({ description: "Patient's first name", example: 'Adam' })
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-zA-Z]+$/, {
+    message: 'Incorrect first name!',
+  })
+  firstName: string;
+
+  @ApiProperty({ description: "Patient's last name", example: 'Smith' })
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-zA-Z]+$/, {
+    message: 'Incorrect last name!',
+  })
+  lastName: string;
+
+  @ApiProperty({
+    description: 'Email of the patient',
+    example: 'alex@gmail.com',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({ description: "Patient's gender", example: 'male' })
+  @IsEnum(Gender)
+  @IsNotEmpty()
+  role: Gender;
+
+  @ApiProperty({
+    description: "Patient's date of birth",
+    example: '1985-04-11',
+  })
+  @IsDateString()
+  @IsNotEmpty()
+  dateOfBirth: Date;
+
+  @ApiProperty({
+    description: "Patient's country",
+    example: 'USA',
+  })
+  @IsString()
+  @IsNotEmpty()
+  country: string;
+
+  @ApiProperty({
+    description: "Patient's city",
+    example: 'Kingston',
+  })
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+
+  @ApiProperty({
+    description: 'Phone number of the patient',
+    example: '380991201212',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[0-9]{0,14}$/, {
+    message: 'Incorrect phone number!',
+  })
+  phoneNumber: string;
+
+  @ApiProperty({
+    description: "Patient's overview",
+    example: 'Allergy to ambrosia',
+  })
+  @IsString()
+  overview: string;
+}
