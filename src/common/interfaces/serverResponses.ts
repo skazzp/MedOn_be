e9c19129767ce1199b3f4814e64686f9c39f9459
WhiteAdam <jurchenko.a@gmail.com>
2023-04-24 14:42:53 +0200

    fix: protect /patients route

--- src/common/interfaces/serverResponses.ts
+++ src/common/interfaces/serverResponses.ts
@@ -2,6 +2,6 @@ import { HttpStatus } from '@nestjs/common';
 
 export interface IServerResponse<T = undefined> {
   statusCode: HttpStatus;
-  message?: string;
+  message?: string | string[];
   data?: T;
 }
--- src/modules/patients/patients.controller.ts
+++ src/modules/patients/patients.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { IServerResponse } from '@common/interfaces/serverResponses';
 import { CreatePatientDto } from '@modules/patients/dto/create-patient.dto';
 import { Patient } from '@entities/Patient';
+import { AuthGuard } from '@nestjs/passport';
 import { PatientsService } from './patients.service';
 
 @Controller('patients')
@@ -15,6 +16,7 @@ export class PatientsController {
     status: 201,
     description: 'Patient was created',
   })
+  @UseGuards(AuthGuard('jwt'))
   async addPatient(
     @Body() dto: CreatePatientDto,
   ): Promise<IServerResponse<Patient>> {
