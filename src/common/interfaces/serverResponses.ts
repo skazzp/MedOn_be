17833bb862c281b84b85946b188aba701e89a7a4
WhiteAdam <jurchenko.a@gmail.com>
2023-04-24 19:23:52 +0200

    fix: default type for IServerResponse

--- src/common/interfaces/serverResponses.ts
+++ src/common/interfaces/serverResponses.ts
@@ -1,6 +1,6 @@
 import { HttpStatus } from '@nestjs/common';
 
-export interface IServerResponse<T = undefined> {
+export interface IServerResponse<T = object> {
   statusCode: HttpStatus;
   message?: string | string[];
   data?: T;
