321bb339e1b6217c26d70a5346bbbe6e46647924
Matheus <matheus-rodrigues37@live.com>
2023-05-06 16:27:43 -0300

    MED-101 chore: refactor controller

--- src/modules/availability/availability.controller.ts
+++ src/modules/availability/availability.controller.ts
@@ -6,12 +6,18 @@ import {
   Delete,
   UseGuards,
   Request,
+  HttpStatus,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiTags, ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+
 import { Roles } from '@decorators/roles.decorator';
+
+import { Availability } from '@entities/Availability';
+
 import { Role } from '@common/enums';
 import { AvailabilityReq } from '@common/interfaces/Availability';
+import { IServerResponse } from '@common/interfaces/serverResponses';
 
 import { RolesGuard } from 'src/guards/roles.guard';
 import { AvailabilityService } from './availability.service';
@@ -28,34 +34,57 @@ export class AvailabilityController {
   @Roles(Role.RemoteDoctor)
   @ApiOperation({ summary: 'Create a new availability' })
   @Post()
-  create(
-    @Body() createAvailabilityDto: CreateAvailabilityDto[],
+  async create(
+    @Body() dto: CreateAvailabilityDto[],
     @Request() req: AvailabilityReq,
-  ) {
-    return this.availabilityService.createMultiples(
-      createAvailabilityDto,
-      +req.user.userId,
-    );
+  ): Promise<IServerResponse<Availability[]>> {
+    await this.availabilityService.createMultiples(dto, req.user.userId);
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: 'Availability was created',
+    };
   }
 
   @ApiOperation({ summary: 'Get a list of all availabilities' })
   @Get()
-  findAll(@Request() req: AvailabilityReq, @Body() dto: { timezone: string }) {
-    return this.availabilityService.findAvailabilities(
-      +req.user.userId,
+  async findAll(
+    @Request() req: AvailabilityReq,
+    @Body() dto: { timezone: string },
+  ): Promise<IServerResponse<Availability[]>> {
+    const availabilities = await this.availabilityService.findAvailabilities(
+      req.user.userId,
       dto.timezone,
     );
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Availabilities were found',
+      data: availabilities,
+    };
   }
 
   @ApiOperation({ summary: 'Get an availability by Day' })
   @Get('day')
-  getByDay(@Body() dto: { day: string; timezone: string }) {
-    return this.availabilityService.getAvailabilityByDay(dto.day, dto.timezone);
+  async getByDay(
+    @Body() dto: { day: string; timezone: string },
+  ): Promise<IServerResponse<Availability[]>> {
+    const availabilities = await this.availabilityService.getAvailabilityByDay(
+      dto.day,
+      dto.timezone,
+    );
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Availabilities were found',
+      data: availabilities,
+    };
   }
 
   @ApiOperation({ summary: "Remove an availability by array of ID's " })
   @Delete()
-  remove(@Body() dto: { id: number[] }) {
-    return this.availabilityService.remove(dto.id);
+  async remove(@Body() dto: { id: number[] }): Promise<IServerResponse<void>> {
+    await this.availabilityService.remove(dto.id);
+    return {
+      statusCode: HttpStatus.NO_CONTENT,
+      message: 'Availabilities were removed',
+    };
   }
 }
