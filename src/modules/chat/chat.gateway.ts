7c454dc48449b5cf1c4e6f0cf494271ef4265279
WhiteAdam <jurchenko.a@gmail.com>
2023-05-20 22:53:06 +0200

    feat: add Logger

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -6,6 +6,7 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
 } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
 import { ChatService } from '@modules/chat/chat.service';
 import { AppointmentsService } from '@modules/appointments/appointments.service';
@@ -18,35 +19,37 @@ export class ChatGateway
   @WebSocketServer()
   server: Server;
 
+  private readonly logger = new Logger(ChatGateway.name);
+
   constructor(
     private appointment: AppointmentsService,
     private chat: ChatService,
   ) {}
 
   afterInit() {
-    console.log('WebSocket gateway initialized');
+    this.logger.log('WebSocket gateway initialized');
   }
 
   handleConnection(client: Socket) {
-    console.log(`Client connected: ${client.id}`);
+    this.logger.log(`Client connected: ${client.id}`);
   }
 
   handleDisconnect(client: Socket) {
-    console.log(`Client disconnected: ${client.id}`);
+    this.logger.log(`Client disconnected: ${client.id}`);
   }
 
   @SubscribeMessage('joinRoom')
   handleJoinRoom(client: Socket, data: { appointmentId: number }) {
     const room = `appointment_${data.appointmentId}`;
     client.join(room);
-    console.log(`Client ${client.id} joined appointment room ${room}`);
+    this.logger.log(`Client ${client.id} joined appointment room ${room}`);
   }
 
   @SubscribeMessage('leaveRoom')
   handleLeaveRoom(client: Socket, data: { appointmentId: number }) {
     const room = `appointment_${data.appointmentId}`;
     client.leave(room);
-    console.log(`Client ${client.id} left appointment room ${room}`);
+    this.logger.log(`Client ${client.id} left appointment room ${room}`);
   }
 
   @SubscribeMessage('sendMessage')
@@ -55,6 +58,6 @@ export class ChatGateway
     const room = `appointment-${dto.appointmentId}`;
     this.server.to(room).emit('newMessage', chat.message);
 
-    console.log(`Client ${client.id} joined appointment room ${room}`);
+    this.logger.log(`Client ${client.id} joined appointment room ${room}`);
   }
 }
